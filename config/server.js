0374c703136f55a52f732406033e6fe37f234318
Francesco Vattiato <fvattiato@movia.co>
2020-10-30 23:09:01 +0100

    recover from backup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6a7df8de28c74960c5dd016ab40c44a5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd70ad434-a1c3-4a5d-a1eb-bc9e653bb7b0'
+};
